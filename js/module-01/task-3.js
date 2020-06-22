4b3ac244325f08525b5e1901e8c52c809eab3241
Peter Shevchuk <raf02041994@gmail.com>
2020-06-22 15:58:00 +0300

    end module 1-5

---
+++ js/module-01/task-3.js
@@ -0,0 +1,18 @@
+const ADMIN_PASSWORD = '123';
+let message;
+
+message = prompt('Введіть пароль');
+
+if (message === null) {
+
+    message = 'Скасовано користувачем!';
+
+} else if (message === ADMIN_PASSWORD) {
+    message = 'Ласкаво просимо!';
+
+} else {
+    message = 'Доступ заборонений, невірний пароль!';
+}
+
+alert(message);
+
---
+++ js/module-01/task-4.js
@@ -0,0 +1,26 @@
+const credits = 23580;
+const pricePerDroid = 3000;     
+let totalDroid;
+let message;
+let totalPrice;
+
+
+totalDroid = prompt('Введіть кількість дроїдів, яку хоче купити');
+
+if (totalDroid === null) {
+
+    message = 'Скасовано користувачем!';
+
+} else if (Number(totalDroid) > 0) {
+    totalPrice = credits - (totalDroid * pricePerDroid);
+
+    if (totalPrice < 0 ) {
+        message = 'Недостатньо коштів на рахунку!';
+    } else {
+        message = 'Ви купили '+totalDroid+' дроїдів, на рахунку залишилося '+totalPrice+' кредитів.'
+    }
+} else {
+    message = 'Невірно введена кількість';
+}
+
+alert(message);
