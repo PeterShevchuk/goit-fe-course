ac0d712576bcd984a1cd6f61b13081f88f69faf3
Peter Shevchuk <raf02041994@gmail.com>
2020-06-23 15:53:00 +0300

    edit 6 task

--- js/module-01/task-6.js
+++ js/module-01/task-6.js
@@ -1,12 +1,10 @@
 'use strict';
 
-let input = true;
+let input = 0;
 let total = 0;
 
-    for (total = 0; Boolean(input) !== false; total+=input) {
-      input = Number(prompt('Введіть будь яке число: '));
-      if (Boolean(input) === false) {
-        alert('Загальна сума чисел дорівнює: '+total);
-        break;
-      }       
-    }
+do {
+  total += parseInt(input);
+  input = prompt('ведіть число:');
+} while (input != null)
+alert('Загальна сума чисел дорівнює ' + total);
