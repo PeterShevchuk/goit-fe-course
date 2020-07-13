da1929aa9e94224b8c1b4f7e22f4ea6137fa1f68
Peter Shevchuk <raf02041994@gmail.com>
2020-07-13 16:59:22 +0300

    end module 08

---
+++ js/module-08/js/app.js
@@ -0,0 +1,71 @@
+import imgArray from "./gallery-items.js";
+
+const gallery = {
+  modalImg: document.querySelector("img.lightbox__image"),
+  listImg: document.querySelector("ul.js-gallery"),
+  modalWindowClose: document.querySelector(".js-lightbox"),
+  modalWindow: document.querySelector(".lightbox"),
+  infoPrevNext: '',
+
+  addImageGallery(prev, orig, desc) {
+    let li = document.createElement("li");
+    let link = document.createElement("a");
+    let img = document.createElement("img");
+
+    li.classList.add("gallery__item");
+    link.classList.add("gallery__link");
+    img.classList.add("gallery__image");
+
+    img.setAttribute("src", prev);
+    img.setAttribute("data-source", orig);
+    img.setAttribute("alt", desc);
+
+    link.appendChild(img);
+    li.appendChild(link);
+
+    return li;
+  },
+  getImagesList(array) {
+    return array.map((obj) =>
+      this.addImageGallery(obj.preview, obj.original, obj.description)
+    );
+  },
+  setImageInGallery(list) {
+    list.append(...this.getImagesList(imgArray));
+  },
+  modalShow(item) {
+    this.modalImg.setAttribute("src", item.getAttribute("data-source"));
+    this.modalWindow.classList.add("is-open");
+    this.infoPrevNext = item.parentNode.parentNode;
+  },
+  modalHide() {
+    this.modalWindow.classList.contains("is-open")? this.modalWindow.classList.remove("is-open") : null
+    this.clearWindow();
+  },
+  clearWindow() {
+    this.modalImg.setAttribute("src", "");
+  },
+  keyDown(event) {
+      let code = event.code;
+      code === 'Escape'? this.modalHide() : null
+      code === 'ArrowLeft'? this.modalPrevNext('prev') : null
+      code === 'ArrowRight'? this.modalPrevNext('next') : null
+  },
+  modalPrevNext(item) {
+    this.modalHide();
+    if (item ==='prev') {this.modalShow(this.infoPrevNext.previousSibling? this.infoPrevNext.previousSibling.querySelector("a").querySelector("img") : this.modalHide())}
+    if (item ==='next') {this.modalShow(this.infoPrevNext.nextSibling? this.infoPrevNext.nextSibling.querySelector("a").querySelector("img") : this.modalHide())}
+  },
+};
+
+gallery.setImageInGallery(gallery.listImg);
+
+gallery.listImg.addEventListener("click", (e) => {
+  gallery.modalShow(e.target);
+});
+
+gallery.modalWindowClose.addEventListener("click", () => {
+  gallery.modalHide();
+});
+
+document.addEventListener('keydown', event => gallery.keyDown(event));
