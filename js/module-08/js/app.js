40d842c621cc60e884475c985d8793265ac14051
Peter Shevchuk <raf02041994@gmail.com>
2020-07-14 18:30:54 +0300

    add info

--- js/module-08/js/app.js
+++ js/module-08/js/app.js
@@ -1,6 +1,7 @@
 import imgArray from "./gallery-items.js";
 
 const gallery = {
+  // Змінні
   modalImg: document.querySelector("img.lightbox__image"),
   listImg: document.querySelector("ul.js-gallery"),
   modalWindowClose: document.querySelector(".js-lightbox"),
@@ -8,6 +9,7 @@ const gallery = {
   infoPrevNext: {}, // info for button prev or next
   button: {prev: 'ArrowLeft', next: 'ArrowRight', esc: 'Escape'},
 
+  // Формування li
   addImageGallery(prev, orig, desc) {
     let li = document.createElement("li");
     let link = document.createElement("a");
@@ -27,38 +29,54 @@ const gallery = {
 
     return li;
   },
+
+  // Отримання масиву li
   getImagesList(array) {
     return array.map((obj) =>
       this.addImageGallery(obj.preview, obj.original, obj.description)
     );
   },
+
+  // Додавання масиву li в ul
   setImageInGallery(array, list) {
     list.append(...this.getImagesList(array));
   },
+
+  // Отримання посилання на картинку та запис в модальне вікно, відкриття модального вікна
   modalShow(item) {
     this.clearWindow();
-    this.modalSave(item);
+    this.modalSave(item); 
     this.modalImg.setAttribute("src", item.getAttribute("data-source"));
     this.modalWindow.classList.add("is-open");
   },
+
+  // Закриття модального вікна
   modalHide() {
     this.modalWindow.classList.contains("is-open")? this.modalWindow.classList.remove("is-open") : null
     this.clearWindow();
   },
+
+  // Очистка модального вікна
   clearWindow() {
     this.modalImg.setAttribute("src", "");
     this.modalImg.setAttribute("alt", "");
     this.infoPrevNext[this.button.prev] = null;
     this.infoPrevNext[this.button.next] = null;
   },
+
+  // Навігація по кнопкам (вліво, вправо та esc)
   keyDown(event) {
       let code = event.code;
       code === this.button.esc? this.modalHide() :  null
       code === this.button.prev || code === this.button.next? this.modalPrevNext(code) : null
   },
+
+  // Навігація наступне зображення та попереднє
   modalPrevNext(item) {
     item === this.button.prev && this.infoPrevNext[item]  || item === this.button.next  && this.infoPrevNext[item]? this.modalShow(this.infoPrevNext[item]) : null
   },
+
+  // Збереження посилання на попереднє та наступне зображення для навігації між ними
   modalSave(item) {
     item = item.parentNode.parentNode;
     this.infoPrevNext[this.button.prev] = item.previousSibling? item.previousSibling.querySelector("a").querySelector("img") : null;
@@ -66,14 +84,18 @@ const gallery = {
   },
 };
 
+// Запуск формування зображень
 gallery.setImageInGallery(imgArray, gallery.listImg);
 
+// При кліку на зображення, воно відкривається в модільному вікні
 gallery.listImg.addEventListener("click", (e) => {
   gallery.modalShow(e.target);
 });
 
+// Клік в будь якому місці щоб закрити модільне вікно
 gallery.modalWindowClose.addEventListener("click", () => {
   gallery.modalHide();
 });
 
+// Отримання клавіші на яку натиснув користувач
 document.addEventListener('keydown', event => gallery.keyDown(event));
